f7ec849e2d6050eeca0d48786eadeef5734f4655
sunrekay <wladkorostilev@yandex.ru>
2022-10-13 00:55:03 +0300

    Добавил тесты для add_question

--- qustion_database_test.py
+++ qustion_database_test.py
@@ -10,8 +10,21 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(question_database.get_question(2), {"word": "разочаровываться",
                                                              "question": "Герцогиня Англии XVIII века Сара Черчилль говорила: «Вы молоды, если вы еще способны…»"})
 
-        self.assertEqual(question_database.get_question(3), {"word": "одиночество.",
+        self.assertEqual(question_database.get_question(3), {"word": "одиночество",
                                                              "question": "Ювелиры часто говорят, что бриллиантам необходимо это."})
+
+    def test_add_questions(self):
+            self.assertEqual(question_database.add_question("1",
+                                                            "котопес",
+                                                            "Где-то однажды Явился на свет С лаем и мяуканьем Зверь, каких нет. Назовите зверя."), True)
+
+            self.assertEqual(question_database.add_question("2",
+                                                            "разочаровываться",
+                                                            "Герцогиня Англии XVIII века Сара Черчилль говорила: «Вы молоды, если вы еще способны…»"), True)
+
+            self.assertEqual(question_database.add_question("3",
+                                                            "одиночество",
+                                                            "Ювелиры часто говорят, что бриллиантам необходимо это."), True)
 
 
 if __name__ == '__main__':
