31e2457e6fb549b0154f3b478c10b3d60b5c3f7a
sunrekay <wladkorostilev@yandex.ru>
2022-10-13 01:13:58 +0300

    Создал тесты для console_output

---
+++ console_output_test.py
@@ -0,0 +1,25 @@
+import unittest
+import console_output
+
+
+class MyTestCase(unittest.TestCase):
+    def test_show_welcome(self):
+        self.assertEqual(console_output.show_welcome(), True)
+
+    def test_show_question(self):
+        self.assertEqual(console_output.show_question("Ювелиры часто говорят, что бриллиантам необходимо это."), True)
+        self.assertEqual(console_output.show_question("Где-то однажды Явился на свет С лаем и мяуканьем Зверь, каких нет. Назовите зверя."), True)
+
+    def test_show_answer(self):
+        self.assertEqual(console_output.show_answer("котопес"), True)
+        self.assertEqual(console_output.show_answer("одиночество"), True)
+
+    def test_show_drum(self):
+        self.assertEqual(console_output.show_drum(), True)
+
+    def test_show_menu(self):
+        self.assertEqual(console_output.show_menu(), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
